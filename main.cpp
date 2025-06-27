7543f5da3439df379c67d9e038ac1d79eee3ee02
Alexander Selivanov <work.a.selivanov@gmail.com>
2025-06-27 17:24:39 +0300

    Version 0.0.1
    A line with prompts for opening the menu has been added and the "About" window has been opened and closed by pressing the "ESC" key.

---
+++ main.cpp
@@ -0,0 +1,157 @@
+#include <cstring>
+#include <curses.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define COLOR_HEADER 1
+#define COLOR_MENU 2
+#define COLOR_WINDOW 3
+#define COLOR_HIGHLIGHT 4
+
+const char *file_menu[] = {"New", "Open", "Save", "Exit", NULL};
+const char *edit_menu[] = {"Cut", "Copy", "Paste", NULL};
+
+WINDOW *edit_win = nullptr;
+
+void draw_header();
+void show_about();
+WINDOW* draw_menu_window(const char* title, int height, int width);
+void draw_workspace(string name);
+
+//Реализация функций
+void draw_header() 
+{
+    attron(COLOR_PAIR(COLOR_HEADER));
+    mvhline(0, 0, ' ', COLS);  // Заполнить строку цветом
+    mvprintw(0, 2, "F1 - File");
+    mvprintw(0, 14, "F2 - Edit");
+    mvprintw(0, 26, "F3 - About");
+    mvprintw(0, 40, "F4 - Exit");
+    attroff(COLOR_PAIR(COLOR_HEADER));
+    refresh();
+}
+
+void show_about() 
+{
+    const char* message = "Simple Notepad. Press [ESC] for close this window.";
+    int width = strlen(message) + 6;
+    int height = 5;
+
+    WINDOW* win_about = draw_menu_window("About", height, width);
+    mvwprintw(win_about, 2, 3, "%s", message);
+    wrefresh(win_about);
+
+    // Ждём ESC
+    while (wgetch(win_about) != 27) {}
+
+    delwin(win_about);
+    touchwin(stdscr);
+    refresh();
+    //
+    draw_header();     
+    draw_workspace("Untitled.txt");  
+}
+
+WINDOW* draw_menu_window(const char* title, int height, int width) 
+{
+    int starty = (LINES - height) / 2;
+    int startx = (COLS - width) / 2;
+    WINDOW* win_menu = newwin(height, width, starty, startx);
+    wbkgd(win_menu, COLOR_PAIR(COLOR_WINDOW));
+    box(win_menu, 0, 0);
+    mvwprintw(win_menu, 0, 2, " %s ", title);
+    wrefresh(win_menu);
+    return win_menu;
+}
+ 
+ void draw_workspace(string name)
+{
+    if (!edit_win)
+    {
+        int height = LINES - 3;
+        int width = COLS - 4;
+        int start_y = 2;
+        int start_x = 2;
+
+        edit_win = newwin(height, width, start_y, start_x);
+    }
+
+    werase(edit_win);  
+    box(edit_win, 0, 0);
+
+    string title = " " + name + " ";
+    int width = getmaxx(edit_win);
+    int title_x = (width - title.length()) / 2;
+    mvwprintw(edit_win, 0, title_x, "%s", title.c_str());
+
+    wrefresh(edit_win);
+}
+
+//Точка входа в программу
+int main(int argc, char *argv[])
+{
+    string filename;
+    
+    if(argc == 2)                                    
+    {
+        filename = argv[1];                          
+        
+        if (filename.find(".txt") == string::npos)          
+        {
+            filename += ".txt";                             
+        }
+        
+        ofstream outfile(filename);                         
+        
+        if (!outfile)                                       
+        {
+            cerr << "Error: failed to create a file " << filename << "\n";         
+            return 1;                                                               
+        }                                                               
+    }
+    else
+    {
+        cerr << "Error: the argument was not passed!" << filename << "\n";    
+        filename = "Untitled.txt";
+    }
+    
+    initscr();
+    noecho();
+    cbreak();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    
+    if (has_colors()) 
+    {
+        start_color();
+        init_pair(COLOR_HEADER, COLOR_WHITE, COLOR_BLUE);
+        init_pair(COLOR_MENU, COLOR_WHITE, COLOR_BLACK);
+        init_pair(COLOR_WINDOW, COLOR_BLACK, COLOR_WHITE);
+        init_pair(COLOR_HIGHLIGHT, COLOR_BLACK, COLOR_CYAN);
+    }
+    
+    draw_header();
+    draw_workspace(filename);
+    
+    int ch;
+    
+    while((ch = getch()) != KEY_F(4))
+    {
+        switch(ch)
+        {
+            case KEY_F(3):
+                show_about(); 
+            break;
+        }
+    }
+    
+    //draw_header();
+    //draw_workspace(filename);
+    
+    if (edit_win)
+        delwin(edit_win);
+    endwin();
+    return 0;
+}

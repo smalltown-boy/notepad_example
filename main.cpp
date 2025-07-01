32ca6a4aff86d64d14aafd4a8dda8ec663390d11
Alexander Selivanov <work.a.selivanov@gmail.com>
2025-07-01 18:36:27 +0300

    Version 0.0.5
    Some functions and logic of the program have been redesigned. Added a window for the F4 key.

--- main.cpp
+++ main.cpp
@@ -5,31 +5,33 @@
 
 using namespace std;
 
+
 #define COLOR_HEADER 1
-#define COLOR_MENU 2
-#define COLOR_WINDOW 3
+#define COLOR_MENU 3
+#define COLOR_WINDOW 2
 #define COLOR_HIGHLIGHT 4
 
-const char *file_menu[] = {"New", "Open", "Save", "Exit", NULL};
+
+const char *file_menu[] = {"New", "Open", "Save", NULL};
 const char *edit_menu[] = {"Cut", "Copy", "Paste", NULL};
 
 WINDOW *edit_win = nullptr;
 
 string filename;
 
-
-void show_about();
+//Прототипы функций
+void show_about(void);
+int show_exit(void);
 void show_submenu(const char** items, const char* title);
-
 WINDOW* draw_menu_window(const char* title, int height, int width);
 void draw_workspace(string name);
-void draw_header();
+void draw_header(void);
 
 //Реализация функций
-void draw_header() 
+void draw_header(void) 
 {
     attron(COLOR_PAIR(COLOR_HEADER));
-    mvhline(0, 0, ' ', COLS);  // Заполнить строку цветом
+    mvhline(0, 0, ' ', COLS);  
     mvprintw(0, 2, "F1 - File");
     mvprintw(0, 14, "F2 - Edit");
     mvprintw(0, 26, "F3 - About");
@@ -38,7 +40,32 @@ void draw_header()
     refresh();
 }
 
-void show_about() 
+int show_exit(void)
+{
+    int key = 0;
+    
+    const char* message = "Are you sure you want to log out? [y/n]";
+    int width = strlen(message) + 6;
+    int height = 5;
+
+    WINDOW* win_exit = draw_menu_window("Attention", height, width);
+    mvwprintw(win_exit, 2, 3, "%s", message);
+    wrefresh(win_exit);
+
+    while(key != 121 && key != 110)
+    {
+        key = wgetch(win_exit);
+    }
+
+    delwin(win_exit);
+    touchwin(stdscr);
+    refresh();
+    draw_header();
+    //draw_workspace(filename);
+    return key;
+}
+
+void show_about(void) 
 {
     const char* message = "Simple Notepad. Press [ESC] for close this window.";
     int width = strlen(message) + 6;
@@ -48,13 +75,11 @@ void show_about()
     mvwprintw(win_about, 2, 3, "%s", message);
     wrefresh(win_about);
 
-    // Ждём ESC
     while (wgetch(win_about) != 27) {}
 
     delwin(win_about);
     touchwin(stdscr);
     refresh();
-    //
     draw_header();     
     draw_workspace(filename);  
 }
@@ -71,14 +96,14 @@ WINDOW* draw_menu_window(const char* title, int height, int width)
     return win_menu;
 }
  
- void draw_workspace(string name)
+void draw_workspace(string name)
 {
     if (!edit_win)
     {
-        int height = LINES - 3;
-        int width = COLS - 4;
-        int start_y = 2;
-        int start_x = 2;
+        int height = LINES - 3; 
+        int width = COLS - 4;   
+        int start_y = 2; 
+        int start_x = 2; 
 
         edit_win = newwin(height, width, start_y, start_x);
     }
@@ -90,7 +115,7 @@ WINDOW* draw_menu_window(const char* title, int height, int width)
     int width = getmaxx(edit_win);
     int title_x = (width - title.length()) / 2;
     mvwprintw(edit_win, 0, title_x, "%s", title.c_str());
-
+    
     wrefresh(edit_win);
 }
 
@@ -134,7 +159,20 @@ void show_submenu(const char** items, const char* title)
             choice = (choice + 1) % count;
         else if (ch == '\n') 
         {
-            break;
+            switch(choice)
+            {
+                case 0:
+                break;
+                
+                case 1:
+                break;
+                
+                case 2:
+                break;
+                
+                default:
+                break;
+            }
         }
     }
 
@@ -146,22 +184,25 @@ void show_submenu(const char** items, const char* title)
     draw_workspace(filename); 
     curs_set(1); 
 }
+ 
 
 //Точка входа в программу
 int main(int argc, char *argv[])
 {
+    int key;
+    
     if(argc == 2)                                    
     {
         filename = argv[1];                          
         
-        if (filename.find(".txt") == string::npos)          
+        if(filename.find(".txt") == string::npos)          
         {
             filename += ".txt";                             
         }
         
         ofstream outfile(filename);                         
         
-        if (!outfile)                                       
+        if(!outfile)                                       
         {
             cerr << "Error: failed to create a file " << filename << "\n";         
             return 1;                                                               
@@ -169,8 +210,8 @@ int main(int argc, char *argv[])
     }
     else
     {
-        cerr << "Error: the argument was not passed!" << filename << "\n";    
         filename = "Untitled.txt";
+        cerr << "Error: the argument was not passed!" << filename << "\n";    
     }
     
     initscr();
@@ -188,15 +229,18 @@ int main(int argc, char *argv[])
     }
     
     draw_header();
+    
     draw_workspace(filename);
     wmove(edit_win, 2, 2);
+    //curs_set(1);
     refresh();
     
-    int ch;
     
-    while((ch = getch()) != KEY_F(4))
+    while(1)
     {
-        switch(ch)
+        key = getch();
+        
+        switch(key)
         {
             case KEY_F(1):
                 show_submenu(file_menu, "File");
@@ -210,21 +254,18 @@ int main(int argc, char *argv[])
                 show_about(); 
             break;
             
+            case KEY_F(4):
+                if(show_exit() == 121)
+                {
+                    return 0;
+                }
+            break;
+            
             default:
-                waddch(edit_win, ch);
+                draw_workspace(filename); 
+                waddch(edit_win, key);
                 wrefresh(edit_win);
             break;
         }
     }
-    
-    //draw_header();
-    //draw_workspace(filename);
-    
-    /*
-    if (edit_win)
-        delwin(edit_win);
-    endwin();
-     * */
-    
-    return 0;
 }
